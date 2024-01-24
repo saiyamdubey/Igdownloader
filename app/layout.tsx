cdd78fed3e94c9bde2c7d523c27d5c9e950d285f
SAIYAM DUBEY <saiya@saiyam>
2024-01-24 23:46:28 +0530

    adding app dir

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title:
+    "!Instagram Downloader Video | Images , Reel , DP , Stories , Highlights",
+  description:
+    "Instagram Downloader | Unlock the full potential of your Instagram experience with our versatile Instagram Downloader! Whether you're looking to save captivating videos, stunning images, engaging Reels, profile pictures (DP), captivating Stories, or eye-catching Highlights, our user-friendly tool has got you covered.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+      <div className="z-10 max-w-7xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <h1>Saiyam dubey bfbfnf</h1>
+      </div>
+    </main>
+  );
+}
