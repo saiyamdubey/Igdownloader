01fbfdf8f2dec98b9673f44a314390ee2ed1a6b9
SAIYAM DUBEY <saiya@saiyam>
2024-01-26 18:39:42 +0530

    added shandcn library

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-5">
       <div className="z-10 max-w-7xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Saiyam dubey</h1>
+        <h1>Saiyam dubey Loves Aishwarya gupta</h1>
       </div>
     </main>
   );
