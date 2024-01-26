190085980acf629322d94a2340fe3a2cc211831d
SAIYAM DUBEY <saiya@saiyam>
2024-01-27 00:23:11 +0530

    added css

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function DatePickerWithPresets() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-5">
       <div className="z-10 max-w-7xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Saiyam dubey Loves Aishwarya gupta</h1>
+        <h1>Saiyam dubey </h1>
         <p className="text-sm text-muted-foreground">
           Enter your email address.
         </p>
