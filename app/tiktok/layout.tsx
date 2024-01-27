8be29ace62dbd326133582e665f3262aa669e255
saiyamdubey <saiyamdubey8787@gmail.com>
2024-01-27 19:14:35 +0530

    added layout css

--- app/tiktok/layout.tsx
+++ app/tiktok/layout.tsx
@@ -13,9 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
       <Navbar />
       {children}
-    </>
+    </main>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,53 +20,56 @@ function Navbar({}: Props) {
   };
 
   return (
-    <div className=" max-w-9xl w-full items-center justify-evenly font-mono text-lm lg:flex border-orange-400 border-b-[1px]  ">
+    <div className=" max-w-9xl w-full items-center justify-evenly font-mono lg:flex border-orange-400 border-b-[1px]  ">
       <div className="flex flex-row logo mb-3 hover:italic">
-        <h2 className="scroll-m-10  text-3xl font-bold tracking-wider first:mt-0 text-primary">
-          Insta
-        </h2>
-        <h3 className="scroll-m-10 text-3xl font-semibold tracking-widest ">
-          down
-        </h3>
-        <h3 className="scroll-m-10 text-2.5xl font-semibold tracking-widest ">
-          .io
-        </h3>
+        <Link className="hover:border-b-[1px] pb-0" href="/" prefetch={true}>
+          {/* Instagram Downloader */}
+          <h2 className="scroll-m-10  text-3xl font-bold tracking-wider first:mt-0 text-primary">
+            Insta
+          </h2>
+          <h3 className="scroll-m-10 text-3xl font-semibold tracking-widest ">
+            down
+          </h3>
+          <h3 className="scroll-m-10 text-2.5xl font-semibold tracking-widest ">
+            .io
+          </h3>
+        </Link>
       </div>
       <div className="links flex flex-row">
-        <div className="mainlinks ml-16">
-          {/* <ol className="flex my-2 ml-8  [&>li]:mr-5 [&>li]:font-semibold [&>li]:cursor-pointer  [&>li]:border-orange-200"> */}
-          <Link
-            className="hover:border-b-[1px] pb-0"
-            href="/instagram/"
-            prefetch={true}
-          >
-            Instagram Downloader
-          </Link>
+        <div className="mainlinks ml-4">
+          <ol className="flex my-2 ml-8  [&>a]:mr-8 [&>a]:font-semibold [&>a]:cursor-pointer  [&>]:border-orange-200">
+            <Link
+              className="hover:border-b-[1px] pb-0"
+              href="/instagram/"
+              prefetch={true}
+            >
+              Instagram Downloader
+            </Link>
 
-          <Link
-            className="hover:border-b-[1px] pb-0"
-            href="/youtube"
-            prefetch={true}
-          >
-            Youtube Downloader
-          </Link>
+            <Link
+              className="hover:border-b-[1px] pb-0"
+              href="/youtube"
+              prefetch={true}
+            >
+              Youtube Downloader
+            </Link>
 
-          <Link
-            className="hover:border-b-[1px] pb-0"
-            href="/pinterest"
-            prefetch={true}
-          >
-            Pinterest Downloader
-          </Link>
+            <Link
+              className="hover:border-b-[1px] pb-0"
+              href="/pinterest"
+              prefetch={true}
+            >
+              Pinterest Downloader
+            </Link>
 
-          <Link
-            className="hover:border-b-[1px] pb-0"
-            href="/tiktok"
-            prefetch={true}
-          >
-            Tiktok Downloader
-          </Link>
-          {/* </ol> */}
+            <Link
+              className="hover:border-b-[1px] pb-0"
+              href="/tiktok"
+              prefetch={true}
+            >
+              Tiktok Downloader
+            </Link>
+          </ol>
         </div>
         <div className="darkmode mb-2 mt-1">
           <DropdownMenu>
