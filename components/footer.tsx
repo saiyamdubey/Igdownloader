e9bb733526842eb6d9e78a0cef5402cd48f3501d
saiyamdubey <saiyamdubey8787@gmail.com>
2024-01-28 21:13:06 +0530

    footer almost done

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,135 +6,181 @@ import { Input } from "@/components/ui/input";
 
 export default function FooterDemo() {
   return (
-    <footer className="bg-primary">
+    <footer className="sm:overflow-hidden">
       <div
         className="
-        container
         flex flex-col flex-wrap
-        px-4
-        py-16
+        py-8
         mx-auto
         sm:items-center
-        :items-start
         md:flex-row md:flex-nowrap
       "
       >
         <div className="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
-          <Link href={"/"} className="text-2xl text-white">
-            Instadown <sup>.io</sup>
+          <Link className="flex justify-center mb-6" href="/" prefetch={true}>
+            <h2 className="scroll-m-10 sm:text-[26px] 2xl:text-3xl 2xl:font-black tracking-wider first:mt-0 text-primary hover:italic">
+              Insta
+            </h2>
+            <h3 className="scroll-m-10 sm:text-[25px] 2xl:text-3xl font-bold tracking-widest ">
+              down
+            </h3>
+            <h3 className="scroll-m-10 sm:text-[12px]  2xl:text-[14px] font-bold tracking-widest ">
+              .io
+            </h3>
           </Link>
-          <p className="mt-2 text-xs text-justify text-gray-400">
-            Footer is a valuable resource that complements the main content of
-            the website by providing quick links, legal information, and ways to
-            connect, creating a well-rounded and user-friendly experience for
-            visitors.
+          <p className="my-8 text-xs text-justify text-gray-400">
+            Saiyam Dubey (Devloper 🙊) : I am Working on this site just for the
+            Educational purpose , I also want you to contribute on this site on
+            my Github Repository which is mentioned on the page of my github
+            (saiyamdubey) .... 💖
           </p>
           <div className="flex mt-4 sm:flex-col ">
             <Input type="email" placeholder="Email" />
-            <Button variant="outline" className="sm:mt-3 ">Subscribe</Button>
+            <Button variant="outline" className="bg-primary sm:mt-3 ">
+              Subscribe
+            </Button>
           </div>
-          <div className="flex justify-center mt-4 space-x-4 lg:mt-2">
-            <Link href={""}>
+          <div className="flex my-10 justify-center space-x-4">
+            <Link href={"/"}>
               <Facebook className="text-blue-500" />
             </Link>
-            <Link href={""}>
+            <Link href={"/"}>
               <Twitter className="text-sky-300" />
             </Link>
-            <Link href={""}>
+            <Link href={"/"}>
               <Instagram className="text-pink-500" />
             </Link>
-            <Link href={""}>
+            <Link href={"/"}>
               <Linkedin className="text-blue-400" />
             </Link>
           </div>
         </div>
-        <div className="justify-between w-full mt-4 text-center lg:flex">
-          <div className="w-full px-4 lg:w-1/3 md:w-1/2">
-            <h2 className="mb-2 font-bold tracking-widest text-gray-100">
-              Quick Links
+        <div className="justify-between w-dvw mt-4 text-center sm:flex-col">
+          <div className="w-dvw lg:w-1/3 md:w-1/2">
+            <h2 className="mb-4 font-extrabold text-xl tracking-widest text-gray-100">
+              Instagram
             </h2>
             <ul className="mb-8 space-y-2 text-sm list-none">
               <li>
                 <Link href={"/"} className="text-gray-300">
-                  Link 1
+                  Instagram video Download
                 </Link>
               </li>
               <li>
                 <Link href={"/"} className="text-gray-300">
-                  Link 2
+                  Instagram Profile Download
                 </Link>
               </li>
               <li>
                 <Link href={"/"} className="text-gray-300">
-                  Link 3
+                  Instagram Highlights Download
                 </Link>
               </li>
               <li>
                 <Link href={"/"} className="text-gray-300">
-                  Link 4
+                  Instagram Stories Download
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="text-gray-300">
+                  Instagram Carosol Download
                 </Link>
               </li>
             </ul>
           </div>
           <div className="w-full px-4 lg:w-1/3 md:w-1/2">
-            <h2 className="mb-2 font-bold tracking-widest text-gray-100">
-              Quick Links
+            <h2 className="mb-4  font-extrabold text-xl tracking-widest text-gray-100">
+              Youtube
             </h2>
             <ul className="mb-8 space-y-2 text-sm list-none">
               <li>
                 <Link href={"/"} className="text-gray-300">
-                  Link 1
+                  Instant Video Download
                 </Link>
               </li>
               <li>
                 <Link href={"/"} className="text-gray-300">
-                  Link 2
+                  Quality Profile Photo
                 </Link>
               </li>
               <li>
                 <Link href={"/"} className="text-gray-300">
-                  Link 3
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 4
+                  Youtube Shorts Downloder
                 </Link>
               </li>
             </ul>
           </div>
           <div className="w-full px-4 lg:w-1/3 md:w-1/2">
-            <h2 className="mb-2 font-bold tracking-widest text-gray-100">
-              Quick Links
+            <h2 className="mb-4 font-extrabold text-xl tracking-widest text-gray-100">
+              Facebook
             </h2>
             <ul className="mb-8 space-y-2 text-sm list-none">
               <li>
                 <Link href={"/"} className="text-gray-300">
-                  Link 1
+                  Facbook Videos Download
                 </Link>
               </li>
               <li>
                 <Link href={"/"} className="text-gray-300">
-                  Link 2
+                  Profile Facebook Download
                 </Link>
               </li>
               <li>
                 <Link href={"/"} className="text-gray-300">
-                  Link 3
+                  Shorts Video Downloder
                 </Link>
               </li>
               <li>
                 <Link href={"/"} className="text-gray-300">
-                  Link 4
+                  Stories Downloder Facebook
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="w-full px-4 lg:w-1/3 md:w-1/2">
+            <h2 className="mb-4  font-extrabold text-xl tracking-widest text-gray-100">
+              Tiktok
+            </h2>
+            <ul className="mb-8 space-y-2 text-sm list-none">
+              <li>
+                <Link href={"/"} className="text-gray-300">
+                  Profile Pic Downloder
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="text-gray-300">
+                  Tiktok Video Downloder
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="text-gray-300">
+                  Tiktok Shorts Downloder
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="w-full px-4 lg:w-1/3 md:w-1/2">
+            <h2 className="mb-4  font-extrabold text-xl tracking-widest text-gray-100">
+              Pinterest
+            </h2>
+            <ul className="mb-8 space-y-2 text-sm list-none">
+              <li>
+                <Link href={"/"} className="text-gray-300">
+                  Image Downloader
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"} className="text-gray-300">
+                  pinterest Video Downloder
                 </Link>
               </li>
             </ul>
           </div>
         </div>
       </div>
-      <div className="flex justify-center -mt-12">
-        <p className="text-center text-white pb-2">
-          @2024 All rights reserved by your website.
+      <div className="flex justify-center border-t-2 border-primary">
+        <p className="text-center sm:text-[13px] text-primary py-4">
+          @2024 Ram Ram Bhaiyo .
         </p>
       </div>
     </footer>
