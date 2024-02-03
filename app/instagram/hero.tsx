7083c084e8cc38fc42c3a08b97593af8af10609e
saiyamdubey <saiyamdubey8787@gmail.com>
2024-02-03 11:47:20 +0530

    day started

--- app/instagram/hero.tsx
+++ app/instagram/hero.tsx
@@ -5,8 +5,16 @@ type Props = {};
 function Hero({}: Props) {
   return (
     <>
-      <div>Hero
-        
+      <div className="h-auto flex bg-primary justify-center items-center m-auto w-[90%]">
+        <div className="headlines">
+          <h1 className="">
+            Instagram <span className="">Downloader</span>
+          </h1>
+          <p className="">Download Instagram Video, Photos, Reels, and Profile Pictures with IG Downloader</p>
+        </div>
+        <div className="linksheader">
+            
+        </div>
       </div>
     </>
   );
