b0cc87dc7691e8c76b423e66c9d73e7475e2bbae
saiyamdubey <saiyamdubey8787@gmail.com>
2024-02-03 12:16:47 +0530

    adding headlinks

--- app/instagram/hero.tsx
+++ app/instagram/hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
@@ -5,15 +6,18 @@ type Props = {};
 function Hero({}: Props) {
   return (
     <>
-      <div className="h-auto flex bg-primary justify-center items-center m-auto w-[90%]">
+      <div className="h-[90vh] flex justify-center items-center m-auto w-[90%]">
         <div className="headlines">
           <h1 className="">
             Instagram <span className="">Downloader</span>
           </h1>
-          <p className="">Download Instagram Video, Photos, Reels, and Profile Pictures with IG Downloader</p>
+          <p className="">
+            Download Instagram Video, Photos, Reels, and Profile Pictures with
+            IG Downloader
+          </p>
         </div>
         <div className="linksheader">
-            
+          <Link href="/video">Dashboard</Link>
         </div>
       </div>
     </>
