d5e20794fa6750003fb07bedecd5568f4ff5a847
saiyamdubey <saiyamdubey8787@gmail.com>
2024-02-12 23:16:46 +0530

    Remove Pinterest and Tiktok pages and layouts. (generated by blackbox.ai )

--- app/pinterest/description.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-function Description({}: Props) {
-  return <div>pinterest Description</div>;
-}
-
-export default Description;
--- app/pinterest/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-
-type Props = {};
-
-export default function Page({}: Props) {
-  return <div>Pinterest page</div>;
-}
