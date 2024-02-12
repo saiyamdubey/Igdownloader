fa8bbe8134a97bdf127610b927f32b1948a50e4c
saiyamdubey <saiyamdubey8787@gmail.com>
2024-02-12 19:19:05 +0530

    removed lang

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html>
       <body className="font-serif overflow-x-hidden">
         <ThemeProvider
           attribute="class"
