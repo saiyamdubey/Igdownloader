4252ac40ddf89d389dd4aadb4cd39a025a25f89e
Dev_Saiyam <70647163+saiyamdubey@users.noreply.github.com>
2024-09-20 23:33:46 +0530

    Add files via upload

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,30 +1,30 @@
-<<<<<<< HEAD
-import React from "react";
+// <<<<<<< HEAD
+// import React from "react";
 
-const Loader: React.FC = () => {
-  return (
-    <>
-      {/* <div className="flex flex-row gap-2  ">
-        <div className="w-8 h-8 rounded-full bg-red-600 animate-ping"></div>
-        <div className="w-8 h-8 rounded-full bg-blue-600 animate-ping [animation-delay:-.3s]"></div>
-        <div className="w-8 h-8 rounded-full bg-purple-600 animate-ping [animation-delay:-.6s]"></div>
-        <div className="w-8 h-8 rounded-full bg-blue-600 animate-ping [animation-delay:-.9s]"></div>
-        <div className="w-8 h-8 rounded-full bg-red-600 animate-ping [animation-delay:-1s]"></div>
-      </div> */}
-      <div className="flex flex-row gap-4 ">
-        <div className="w-8 h-15 rounded-full bg-blue-700 animate-bounce "></div>
-        <div className="w-8 h-15 rounded-full bg-blue-700 animate-bounce [animation-delay:0.2s]"></div>
-        <div className="w-8 h-8 rounded-full bg-blue-600 animate-bounce [animation-delay:.4s]"></div>
-        <div className="w-8 h-8 rounded-full bg-blue-600 animate-bounce [animation-delay:.6s]"></div>
-        <div className="w-8 h-8 rounded-full bg-blue-600 animate-bounce [animation-delay:.8s]"></div>
-        <div className="w-8 h-8 rounded-full bg-blue-600 animate-bounce [animation-delay:1s]"></div>
-      </div>
-    </>
-  );
-};
+// const Loader: React.FC = () => {
+//   return (
+//     <>
+//       {/* <div className="flex flex-row gap-2  ">
+//         <div className="w-8 h-8 rounded-full bg-red-600 animate-ping"></div>
+//         <div className="w-8 h-8 rounded-full bg-blue-600 animate-ping [animation-delay:-.3s]"></div>
+//         <div className="w-8 h-8 rounded-full bg-purple-600 animate-ping [animation-delay:-.6s]"></div>
+//         <div className="w-8 h-8 rounded-full bg-blue-600 animate-ping [animation-delay:-.9s]"></div>
+//         <div className="w-8 h-8 rounded-full bg-red-600 animate-ping [animation-delay:-1s]"></div>
+//       </div> */}
+//       <div className="flex flex-row gap-4 ">
+//         <div className="w-8 h-15 rounded-full bg-blue-700 animate-bounce "></div>
+//         <div className="w-8 h-15 rounded-full bg-blue-700 animate-bounce [animation-delay:0.2s]"></div>
+//         <div className="w-8 h-8 rounded-full bg-blue-600 animate-bounce [animation-delay:.4s]"></div>
+//         <div className="w-8 h-8 rounded-full bg-blue-600 animate-bounce [animation-delay:.6s]"></div>
+//         <div className="w-8 h-8 rounded-full bg-blue-600 animate-bounce [animation-delay:.8s]"></div>
+//         <div className="w-8 h-8 rounded-full bg-blue-600 animate-bounce [animation-delay:1s]"></div>
+//       </div>
+//     </>
+//   );
+// };
 
-export default Loader;
-=======
+// export default Loader;
+// =======
 import React from "react";
 
 const Loader: React.FC = () => {
@@ -48,4 +48,4 @@ const Loader: React.FC = () => {
 };
 
 export default Loader;
->>>>>>> bec30cfea9a92640a262797ae437a618468dfd2e
+// >>>>>>> bec30cfea9a92640a262797ae437a618468dfd2e
